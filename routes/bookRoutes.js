c87761ef2db97deca03fd95093a25a13cbddae15
Yogesh Shinde <yogeshshinde3624@gmail.com>
2025-04-06 19:34:36 +0530

    Update and delete api done

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -16,12 +16,57 @@ router.post('/addbook', async (req, res) => {
 
 router.get('/getbooks', async (req, res) => {
    try {
-     const books = await bookSchema.find();
-     res.json(books);
+      const books = await bookSchema.find();
+      res.json(books);
    } catch (err) {
-     res.status(500).json({ error: 'Error fetching books' });
+      res.status(500).json({ error: 'Error fetching books' });
    }
- });
+});
 
+router.get('/getbook/:id', async (req, res) => {
+   try {
+      const book = await bookSchema.findById(req.params.id);
+      if (!book) {
+         return res.status(404).json({ error: 'Book not found' });
+      }
+      res.json(book);
+   } catch (err) {
+      res.status(500).json({ error: err });
+   }
+}
+);
+
+router.delete('/deletbook/:id', async (req, res) => {
+   try {
+      const findbook = await bookSchema.findById(req.params.id);
+      if (!findbook) {
+         return res.status(404).json({ error: 'Book not found' });
+      }
+      const book = await bookSchema.deleteOne({ _id: req.params.id });
+      res.json("Book delete ✅");
+   } catch (err) {
+      res.status(500).json({ error: err })
+   }
+
+});
+
+router.patch('/updatebook/:id', async (req, res) => {
+   try {
+      const findBook = await bookSchema.findById(req.params.id);
+      if (!findBook) {
+         return res.status(404).json({ error: 'Book not found' });
+      }
+
+      const updatedBook = await bookSchema.updateOne(
+         { _id: req.params.id },     // Filter
+         { $set: req.body }          // Update
+      );
+
+      res.json({ message: 'Book updated successfully' });
+
+   } catch (err) {
+      res.status(500).json({ Error: err });
+   }
+})
 
 module.exports = router;
